4abf3c018ce7fbed83e86482882cf429505e60c4
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2019-09-05 16:42:18 +0530

    Initial Commit

---
+++ Xamarin_Test_1/Xamarin_Test_1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin_Test_1.Models;
+
+namespace Xamarin_Test_1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
